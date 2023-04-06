2ab268aaabd6030ad5eb1267f1467aa8b96216dd
comit teste <christofer_daniel12@hotmail.com>
2023-04-05 21:59:59 -0300

    teste firebase

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client"
+
+// app
+import App from "./App"
+import LoginScrren from "./LoginScreen"
+
+const root = document.querySelector("#root")
+ReactDOM.createRoot(root).render(<App />)
